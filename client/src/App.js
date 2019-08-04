738ec17bd96201d3ec01a57f2ce3b8810a4382ea
Dameon Brown <axel12gt@live.com>
2019-08-04 16:04:15 -0400

    added concurrently script

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
+import Customers from "./components/Customers"
 import './App.css';
 
 function App() {
@@ -10,15 +11,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </header> 
+      <Customers />
     </div>
   );
 }
