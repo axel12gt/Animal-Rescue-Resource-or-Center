ba1151779866010840532e683a211b454df958bd
Dameon Brown <axel12gt@live.com>
2019-08-05 06:57:48 -0400

    added login and register route skeletons removed ejs and express-ejs dependency

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Test from "./pages/Test";
+import Test from "./pages/Test/Test";
+import Login from "./pages/Login"
+import Register from "./pages/Register"
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <div>
         <Switch>
           <Route exact path="/" component={Test} />
+          <Route exact path="/users/login" component={Login}/>
+          <Route exact path="/users/register" component={Register}/>
         </Switch>
       </div>
     </Router>
--- client/src/pages/Test/Test.js
+++ client/src/pages/Test/Test.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from '../logo.svg';
-import Customers from "../components/customers"
+import logo from '../../logo.svg';
+import Customers from "../../components/customers"
 import './Test.css';
 
 function Test() {
