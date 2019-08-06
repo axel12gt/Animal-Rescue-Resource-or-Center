18fa394c3c13e035e31ffbb6f9c26db3222ca0b9
Brian Corbett <briancorbett@brighthouse.com>
2019-08-06 11:50:13 -0400

    Started seeds file and added footer

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <footer class="app-footer">
+  <div>
+    <a href="#">--</a>
+    <span>&copy; 2019</span>
+  </div>
+  <div class="ml-auto">
+    <span>--</span>
+    <a href="#">--</a>
+  </div>
+</footer>
+    )
+        }
+    }
+
+    export default Footer;
