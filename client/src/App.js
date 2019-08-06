52fad344cfa0e558e330b6afda7da80fb71c4d9f
Brian Corbett <briancorbett@brighthouse.com>
2019-08-06 15:49:29 -0400

    Created homepage files

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Test from "./pages/Test/Test";
-import Login from "./pages/Login"
-import Register from "./pages/Register"
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+// import Footer from "./components/Footer";
+// import Navbar from "./components/navbar"
 
 function App() {
   return (
@@ -12,6 +14,7 @@ function App() {
           <Route exact path="/" component={Test} />
           <Route exact path="/users/login" component={Login}/>
           <Route exact path="/users/register" component={Register}/>
+          
         </Switch>
       </div>
     </Router>
