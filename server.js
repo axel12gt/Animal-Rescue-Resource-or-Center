63e730b71c9c9cab5b31d650fa33aee4e1e60781
Dameon Brown <axel12gt@live.com>
2019-08-08 05:46:12 -0400

    server middleware set up

--- server.js
+++ server.js
@@ -1,28 +1,81 @@
 // when you invoke express as a dependency, you have to immediately call app = express() otherwise express doesnt run properly
 const express = require("express");
-const routes = require("./routes/api");
+const path = require("path");
+const logger = require("morgan");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const passport = require("passpwer");
+const localStrategy = require("passport-local").Strategy;
+const flash = require("connect-flash");
+const routes = require("./routes/api");
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-// Bodyparser
-// this has to be before you process your routes, otherwise your route cant process the json
+// Middleware
+app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json())
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(
+  require("express-session")({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false
+  })
+);
+app.use(passport.initialize());
+app.use(flash());
 
-// Add routes
+// static assets for heroku
+if (process.env.NODE_ENV === "production") {
+  app.use(passport.session());
+  app.use(express.static(path.join(__dirname, "../build")));
+}
+
+// Routes
 app.use(routes);
 
-// mongodb connection
+// Express 404
+app.use((req, res, next) => {
+  let err = new Error("404 in Server.js, route not found");
+  err.status = 404;
+  next(err);
+});
+
+// Mongoose
 mongoose
   .connect(process.env.MONGODB_URI || "mongodb://localhost/animalRescue")
   .then(() => console.log("mongodb connected"))
   .catch(err => console.log(err));
 
+// Error Handling
 
+// Dev Error Handling
+if (app.get("env") === "development") {
+  app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.json({
+      message: err.message,
+      error: err
+    });
+  });
+}
 
+// Prod Error Handling
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    eroor: {}
+  });
+});
+
+// Express listener
 app.listen(PORT, () =>
-  console.log(`server started on http://localhost:${PORT}`)
+  console.log(`server started on ${PORT}`)
 );
